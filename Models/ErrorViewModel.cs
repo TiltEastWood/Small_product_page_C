04f76fc6e3ce157334ce6e7782db60d1e7923e66
Markuss Sloka <markuss1777@gmail.com>
2025-05-24 17:52:09 +0300

    update add prodcut dynamic fields sku logic

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Small_product_page_C_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ViewModels/AddProductViewModels.cs
@@ -0,0 +1,19 @@
+﻿using Small_product_page_C_.Models;
+
+namespace Small_product_page_C_.ViewModels
+{
+    public class AddProductViewModels
+    {
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public ProductType ProductType { get; set; }
+
+        public int? Height { get; set; }
+        public int? Width { get; set; }
+        public int? Length { get; set; }
+        public int? Weight { get; set; }
+        public int? StorageMB { get; set; }
+    }
+}
+   
